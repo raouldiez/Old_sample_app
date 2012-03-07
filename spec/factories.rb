285612c75ce575671511fa693aaa823dc9a38af6
Raoul Diez <contact@raouldiez.com>
2012-03-06 19:31:31 -0800

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do 
+	factory :user do
+
+		name	"John Diez"
+		email	"jd@diez.com"
+		password "foobar"
+	end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,10 +3,26 @@ require 'spec_helper'
 describe "UserPages" do
 	subject {page}
 
-  describe "signup page" do
+  describe "sign up" do
   	before { visit signup_path }
 
-  	it { should have_selector('h1', text: 'Sign up')}
-  	it { should have_selector('title', text: full_title('Sign up'))}
+  	describe "with invalid information" do
+  		it "should not create a user" do
+  			expect { click_button "Sign up"}.not_to change(User, :count)
+  		end
+  	end
+
+  	describe "with valid information" do
+  		before do
+  			fill_in "Name",		with: "Example User"
+  			fill_in "Email",	with: "user@example.com"
+  			fill_in "Password",	with: "foobar"
+  			fill_in "Confirmation", with: "foobar"
+  		end
+
+  		it "should create a user" do
+  			expect { click_button "Sign up" }.to change(User, :count).by(1)
+  		end
+  	end
   end
- end
+end
