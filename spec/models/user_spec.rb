d658159935c372b433a6166dee9f437d9a67d0e4
Raoul Diez <contact@raouldiez.com>
2012-03-08 14:43:54 -0800

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,9 +22,11 @@ describe User do
 
 	it { should respond_to(:name) }
 	it { should respond_to(:email) }
-	it { should respond_to(:password_digest)}
-	it { should respond_to(:password_digest)}
-	it { should respond_to(:password_confirmation)}
+	it { should respond_to(:password_digest) }
+	it { should respond_to(:password_digest) }
+	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:remember_token) }
+	it { should respond_to(:authenticate) }
 
 	it { should be_valid }
 
@@ -100,4 +102,8 @@ describe User do
     	end
   	end
 
+  	describe "remember token" do
+    	before { @user.save }
+    	its(:remember_token) { should_not be_blank }
+   end
 end
